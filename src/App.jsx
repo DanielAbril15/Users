00016c25f3fe6d912fe467a722d4b98bcbabf15b
Daniel Abril <danbril1015@gmail.com>
2022-08-22 09:27:27 -0500

    agrego get, post y delete de users

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import UsersList from "./components/UsersList";
+import PostUsers from "./components/PostUsers";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState();
+
+  const getAllUsers = () => {
+    const URL = "https://users-crud1.herokuapp.com/users/";
+    axios
+      .get(URL)
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    getAllUsers();
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <PostUsers getAllUsers={getAllUsers} />
+      <h1>USERS</h1>
+      <section className="cards__container">
+        {users?.map((user) => (
+          <UsersList user={user} key={user.id} getAllUsers={getAllUsers} />
+        ))}
+      </section>
+      <button className="btn__create">+</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/UsersList.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import React from "react";
+import "./css/usersList.css";
+const UsersList = ({ user, getAllUsers }) => {
+  const deleteUser = () => {
+    const URL = `https://users-crud1.herokuapp.com/users/${user.id}`;
+    axios
+      .delete(URL)
+      .then((res) => getAllUsers())
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <article className="user__card">
+      <p className="user__name">
+        {user["first_name"]} {user["last_name"]}
+      </p>
+
+      <div className="user__info">
+        <div className="info">
+          <p className="info__title">EMAIL</p>
+          <p>{user.email}</p>
+        </div>
+        <div className="info">
+          <p className="info__title">BIRTHDAY</p>
+          <p>{user.birthday}</p>
+        </div>
+      </div>
+
+      <div className="user__btns">
+        <button onClick={deleteUser} className="btn btn__trash">
+          <img src="../../public/imgs/trash.png" alt="trash button" />
+        </button>
+        <button className="btn btn__edit">
+          <img src="../../public/imgs/edit.png" alt="edit button" />
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default UsersList;
